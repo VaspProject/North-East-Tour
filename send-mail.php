fab08ea6f88f9388f7d21608fad76f9febd15595
VaspProject <vaspbhagyashree@gmail.com>
2025-12-12 17:15:49 +0530

    new-change

---
+++ send-mail.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $travel_date = $_POST['travel_date'];
+    $pax = $_POST['pax'];
+    $message = $_POST['message'];
+
+    $to = "northeasttourplanner.in@gmail.com";
+    $subject = "Tour Enquiry from Website";
+
+    $body = "
+    --- New Enquiry Details ---
+
+    Name: $name
+    Phone: $phone
+    Email: $email
+    Travel Date: $travel_date
+    Total Pax: $pax
+    Message:
+    $message
+
+    ---------------------------
+    ";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<h2 style='text-align:center;color:green;'>Enquiry Sent Successfully!</h2>";
+    } else {
+        echo "<h2 style='text-align:center;color:red;'>Failed to Send Enquiry. Try Again.</h2>";
+    }
+}
+?>
