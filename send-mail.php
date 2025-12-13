dd51143777e992d42bc55c5fab94a54a27c3a7f1
VaspProject <vaspbhagyashree@gmail.com>
2025-12-13 13:51:13 +0530

    saturday-update

--- send-mail.php
+++ send-mail.php
@@ -9,7 +9,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST['message'];
 
     $to = "northeasttourplanner.in@gmail.com";
-    $subject = "Tour Enquiry from Website";
+    $subject = "Tour Enquiry";
 
     $body = "
     --- New Enquiry Details ---
